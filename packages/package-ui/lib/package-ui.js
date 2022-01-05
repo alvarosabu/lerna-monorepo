4d519b20d3e43a965ace35fb770c998c61f0b585
Alvaro <alvaro.saburido@gmail.com>
2022-01-05 11:07:55 +0100

    feat(package-ui): add label

--- packages/package-ui/lib/package-ui.js
+++ packages/package-ui/lib/package-ui.js
@@ -9,7 +9,10 @@ function packageUi() {
     const { slugify } = packageUtils();
     const button = `<button>${slugify('Awiwi soy un boton')}</button>`
 
+    const label = `<label>${slugify('Awiwi soy una label')}</label>`
+
     return {
         button,
+        label,
     }
 }
